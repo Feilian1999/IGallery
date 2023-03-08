e930dce6e662fe3bc08bcd4465a977592fde6f9c
Feilian1999 <allen3342@gmail.com>
2023-03-08 22:49:18 +0800

    renew token

--- WebApplication1/Services/GetIgDataService.cs
+++ WebApplication1/Services/GetIgDataService.cs
@@ -11,7 +11,7 @@ namespace WebApplication1.Services
             using (var client = new HttpClient { BaseAddress = new Uri("https://graph.instagram.com") })
             {
                 var fields = "id,caption,media_type,media_url,permalink,thumbnail_url,timestamp,username";
-                var token = "IGQVJWdDRZAbGJWYTFvTDZAtc0xOamxjeTBEdEltbXhadVFUYU1wOHNISFNxRVoweXJDZAmlBM1NKaEtMWXRPLUh5X2hzbkZAOR05qdjZA0cm52ekJtNlRZAQ0ZAWZAGVsZAWQ3dlJnODF5dS13";
+                var token = "IGQVJWQWdWY1NwQks0VWlxVkpMeFhXaWxnR0RqMUhlQlkxOE1MdnF6MnpPTU5vbnAtVkloOUpqd1hjNmI0VnA3OTRQdzFPVmcxd0NOZA09PbGp3cEhsUkZAhS05wR2M3MkNYc0F1OWN3";
                 string url = $"/me/media?fields={fields}&access_token={token}";
 
                 HttpResponseMessage response = await client.GetAsync(url);
